0263c92a8f9c5c8c94efc3f2bcad37641373fda7
CarlosNatanael <carlosnatha2345@gmail.com>
2026-05-15 01:03:47 -0300

    feat: sincronizado tudo para app.py rodar

--- backend/app.py
+++ backend/app.py
@@ -2,16 +2,31 @@ from flask import Flask
 from config import Config
 from database import close_db
 
+# Importação dos Blueprints
+from rotas.auth import auth_bp
+from rotas.admin import admin_bp
+from rotas.manutencao import manutencao_bp
+from rotas.solicitante import solicitante_bp
+
 def create_app():
+
     app = Flask(__name__, 
                 template_folder='../frontend/templates', 
                 static_folder='../frontend/static')
     
     app.config.from_object(Config)
+
     app.teardown_appcontext(close_db)
+
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(admin_bp, url_prefix='/admin')
+    app.register_blueprint(manutencao_bp, url_prefix='/manutencao')
+    app.register_blueprint(solicitante_bp, url_prefix='/solicitante')
 
     return app
 
+# Instância da aplicação
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host="0.0.0.0", port=5000, debug=True)
